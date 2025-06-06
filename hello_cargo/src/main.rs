1ed7d00eb5e3b8672de7b823932f2aebe997d416
Om Sharma <omsharma.83173@gmail.com>
2025-06-06 23:49:36 +0530

    written-a-is_even_program-in-rust.

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+fn main(){
+    println!("Function Calling");
+    println!("{}",is_even(41));
+}
+
+fn is_even(num : i32) -> bool{
+    if num%2 == 0 {
+        return true;
+    }else{
+        return false;
+    }
+
 }
