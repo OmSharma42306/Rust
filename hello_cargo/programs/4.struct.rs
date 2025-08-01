9ea668a83d2fe3fabdcd5d6a6cb4e01101c410ec
Om Sharma <omsharma.83173@gmail.com>
2025-08-01 19:12:09 +0530

    feat:struct-enum-stuff

--- hello_cargo/programs/4.struct.rs
+++ hello_cargo/programs/4.struct.rs
@@ -16,3 +16,81 @@ fn main(){
     println!("new User age is : {} ",new_user.age);
 }
 
+
+
+// other ways
+/*Define a struct called User with fields: username: String, email: String, age: u8.
+
+Create a function print_user(user: &User) that prints user details.
+
+Add a method on User called is_adult() that returns true if age ≥ 18.*/
+
+struct user{
+    username : String,
+    email : String,
+    age : i32
+}
+
+fn main(){
+    let user1 = user{
+        username : String :: from("Om Sharma"),
+        email : String :: from("omsharma.83173@gmail.com"),
+        age : 20
+    };
+
+    print_struct(user1);
+    
+}
+
+fn print_struct(userx : user){
+    println!("Username :  {}",userx.username);
+    println!("Email : {}",userx.email);
+    println!("Age : {}",userx.age);
+}
+
+
+
+// another way
+/*Define a struct called User with fields: username: String, email: String, age: u8.
+
+Create a function print_user(user: &User) that prints user details.
+
+Add a method on User called is_adult() that returns true if age ≥ 18.*/
+
+struct user{
+    username : String,
+    email : String,
+    age : i32
+}
+
+// to implement method to an struct use `impl`
+
+impl user{
+    fn is_adult(&self)->bool{
+        if(self.age > 18){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
+
+fn main(){
+    let user1 = user{
+        username : String :: from("Om Sharma"),
+        email : String :: from("omsharma.83173@gmail.com"),
+        age : 20
+    };
+
+    print_struct(user1);
+    
+    
+}
+
+fn print_struct(userx : user){
+    println!("Username :  {}",userx.username);
+    println!("Email : {}",userx.email);
+    println!("Age : {}",userx.age);
+    
+    println!("Is Adult ? {}",userx.is_adult())
+}
--- hello_cargo/programs/6.enum.rs
+++ hello_cargo/programs/6.enum.rs
@@ -18,3 +18,39 @@ fn move_around(direction:Direction){
     println!("{:?}",direction);
 
 }
+
+
+
+/*
+Define an enum called TrafficLight with variants: Red, Yellow, Green.
+
+Write a function action(light: TrafficLight) that uses match to print:
+
+"Stop" for Red
+
+"Ready" for Yellow
+
+"Go" for Green
+
+Create an enum called Result<T, E> and implement a function divide(a, b) that returns Ok(result) or Err("Divide by zero").
+*/
+#[derive(Debug)]
+#[derive(PartialEq)]
+enum TrafficLight {
+    Red,
+    Yellow,
+    Green
+}
+
+fn action(light : TrafficLight){
+    if(light == TrafficLight :: Red){
+        println!("{:?}",light)
+    }
+    
+}
+
+fn main(){
+    let my_light : TrafficLight = TrafficLight::Green;
+
+    action(my_light);
+}
